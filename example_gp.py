f06a82e555974bc3a82851e341357f941e58ca21
neal-o-r <neal@n-o-r.xyz>
2018-05-11 22:08:37 +0100

    example

---
+++ example_gp.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.spatial.distance import cdist
+import scipy.optimize as opt
+
+def gaussian(C):
+        '''
+        return draw from a 0 mean gaussian with given convariance structure
+        '''
+        return np.random.multivariate_normal(np.zeros(len(C)), C)
+
+
+def sq_exp_kernel(x, x_d, theta=[1, 1]):
+        '''
+        exponentiated quadratic kernel
+        '''
+        sig, l = theta
+        sqdist = (np.sum(x**2, 1).reshape(-1, 1) +
+                  np.sum(x_d**2, 1) -
+                  2 * np.dot(x, x_d.T))
+
+        return sig**2 * np.exp(-0.5 * (1 / l**2) * sqdist)
+
+def periodic_kernel(x, x_d, theta=[1, 1]):
+
+        l, p = theta
+        dists = cdist(x, x_d, metric='euclidean')
+        return np.exp(- 2 * (np.sin(np.pi / p * dists) / l) ** 2)
+
+
+def logl(theta, x, y, u, x_test):
+
+        x = x.reshape(-1, 1)
+        K = sq_exp_kernel(x, x, theta) + np.eye(len(x))*u**2
+        y = y.reshape(-1, 1)
+
+        L =    ( - 0.5 * np.dot(np.dot(y.T, np.linalg.inv(K)), y)
+                 - 0.5 * np.log(np.linalg.det(K + 1e-6 * np.eye(len(x))))
+                 - 0.5 * len(x) * np.log(2 * np.pi))[0][0]
+
+        return L if np.isfinite(L) else 1e25
+
+def f(x):
+
+        return 2*np.sin(3*x) - np.cos(5*x) + x**2
+
+def get_data(n_points):
+
+        x = np.random.uniform(-2, 2, n_points)
+        x.sort()
+        u = 0.5
+        y =  f(x) + np.random.randn(n_points) * u
+
+        return x, y, u
+
+
+def plot_all(x, y, u, x_s, theta):
+
+        kss = sq_exp_kernel(x_s, x_s, theta)
+        ksx = sq_exp_kernel(x_s, x, theta)
+        kxs = sq_exp_kernel(x, x_s, theta)
+        kxx = sq_exp_kernel(x, x, theta)
+        inv = np.linalg.inv
+        o = u**2 * np.eye(len(x))
+
+        mu = ksx.dot(inv(kxx + o)).dot(y)
+        C = kss - ksx.dot(inv(kxx + o)).dot(kxs)
+
+        for i in range(10):
+                plt.plot(x_s, np.random.multivariate_normal(mu.ravel(), C),
+                                 color='#4682b4', lw=1, alpha=0.4)
+
+        plt.errorbar(x.ravel(), y.ravel(), yerr=u, fmt='ok')
+        plt.show()
+
+
+x, y, u = get_data(25)
+x = x.reshape(-1, 1)
+y = y.reshape(-1, 1)
+x_s = np.linspace(-2, 2, 200).reshape(-1,1)
+nll = lambda *args: -1 * logl(*args)
+result = opt.minimize(nll, [1, 1], args=(x, y, u, x_s))
+
+plot_all(x, y, u, x_s, result.x)
+
--- gp.py
+++ gp.py
@@ -2,99 +2,97 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
-plt.style.use('ggplot')
 np.random.seed(123)
 
 def f(x):
 
-	return 2*np.sin(3*x) - np.cos(5*x) + x**2
+        return 2*np.sin(3*x) - np.cos(5*x) + x**2
 
 def get_data(n_points):
 
-	x = np.random.uniform(-2, 2, n_points)
-	x.sort()
-	u = 0.5
-	y =  f(x) + np.random.randn(n_points) * u
+        x = np.random.uniform(-2, 2, n_points)
+        x.sort()
+        u = 0.5
+        y =  f(x) + np.random.randn(n_points) * u
 
-	return x, y, u
+        return x, y, u
 
 
 def plot_data_and_draws(x, y, u, x_t, G, y_b):
 
-	plt.errorbar(x, y, yerr=u, fmt='.k')
-	x_l = np.linspace(x[0], x[-1], 75)
-	#plt.plot(x_l, f(x_l), label='Ground Truth', color='k')
-	plt.plot(x_t, G, color='#4682b4', lw=1, alpha=0.4)
-	plt.plot(x_t, y_b, label='Mean', color='r')
-	plt.legend()
+        plt.errorbar(x, y, yerr=u, fmt='.k')
+        x_l = np.linspace(x[0], x[-1], 75)
+        ax = plt.axes()
+        ax.plot(x_t, G, color='#4682b4', lw=1, alpha=0.4)
+        plt.xlim((x.min(), x.max()))
 
-	plt.show()
+        plt.show()
 
 
 def kernel(x, x_d, theta=[1,1]):
-	
-	sig = theta[0]
-	l   = theta[1]
-	
-	sqdist = np.sum(x**2,1).reshape(-1,1) + np.sum(x_d**2,1) - 2*np.dot(x, x_d.T)
-	return sig**2 * np.exp(-0.5 * (1/l**2) * sqdist)
+
+        sig = theta[0]
+        l   = theta[1]
+
+        sqdist = np.sum(x**2,1).reshape(-1,1) + np.sum(x_d**2,1) - 2*np.dot(x, x_d.T)
+        return sig**2 * np.exp(-0.5 * (1/l**2) * sqdist)
 
 
 def gpr(x, y, u, x_test, params):
-	
-	n_draws = 10	
 
-	x = x.reshape(-1, 1)
-	x_test = x_test.reshape(-1, 1)
+        n_draws = 10
 
-	K    = kernel(x, x, params) + np.eye(len(x))*u**2
-	K_s  = kernel(x, x_test, params)
-	K_ss = kernel(x_test, x_test, params) + u**2
+        x = x.reshape(-1, 1)
+        x_test = x_test.reshape(-1, 1)
 
-	L = np.linalg.cholesky(K)
-	Lk = np.linalg.solve(L, K_s)
-	mu = np.dot(Lk.T, np.linalg.solve(L, y.reshape(-1, 1)))
-	L = np.linalg.cholesky(K_ss + 1e-6*np.eye(len(x_test)) - np.dot(Lk.T, Lk))
-	G = mu.reshape(-1,1) + np.dot(L, np.random.normal(size=(len(x_test), n_draws)))
-	y_bar = np.dot(np.dot(K_s.T, np.linalg.inv(K)), y.reshape(-1, 1))
+        K    = kernel(x, x, params) + np.eye(len(x))*u**2
+        K_s  = kernel(x, x_test, params)
+        K_ss = kernel(x_test, x_test, params) + u**2
 
-	plot_data_and_draws(x, y, u, x_test, G, y_bar)
+        L = np.linalg.cholesky(K)
+        Lk = np.linalg.solve(L, K_s)
+        mu = np.dot(Lk.T, np.linalg.solve(L, y.reshape(-1, 1)))
+        L = np.linalg.cholesky(K_ss + 1e-6*np.eye(len(x_test)) - np.dot(Lk.T, Lk))
+        G = mu.reshape(-1,1) + np.dot(L, np.random.normal(size=(len(x_test), n_draws)))
+        y_bar = np.dot(np.dot(K_s.T, np.linalg.inv(K)), y.reshape(-1, 1))
+
+        plot_data_and_draws(x, y, u, x_test, G, y_bar)
 
 
 def logl(theta, x, y, u, x_test):
 
-	x = x.reshape(-1, 1)	
-	K = kernel(x, x, theta) + np.eye(len(x))*u**2
-	y = y.reshape(-1, 1)
+        x = x.reshape(-1, 1)
+        K = kernel(x, x, theta) + np.eye(len(x))*u**2
+        y = y.reshape(-1, 1)
 
-	L =    ( - 0.5 * np.dot(np.dot(y.T, np.linalg.inv(K)), y) 
-		 - 0.5 * np.log(np.linalg.det(K + 1e-6 * np.eye(len(x)))) 
-		 - 0.5 * len(x) * np.log(2 * np.pi))[0][0]
+        L =    ( - 0.5 * np.dot(np.dot(y.T, np.linalg.inv(K)), y)
+                 - 0.5 * np.log(np.linalg.det(K + 1e-6 * np.eye(len(x))))
+                 - 0.5 * len(x) * np.log(2 * np.pi))[0][0]
 
-	return L if np.isfinite(L) else 1e25
+        return L if np.isfinite(L) else 1e25
 
 
 def opt_params(L, x, y, u, x_test, params):
-	
-	ll = lambda *args: -L(*args) 
 
-	result = opt.minimize(ll, params, args=(x, y, u, x_test))
-	return result['x']
+        ll = lambda *args: -L(*args)
+
+        result = opt.minimize(ll, params, args=(x, y, u, x_test))
+        return result['x']
 
 
 if __name__ == '__main__':
 
-	# Given 10 points, with some uncertainty 
-	n_train = 25
-	x, y, u = get_data(n_train)
+        # Given 10 points, with some uncertainty
+        n_train = 25
+        x, y, u = get_data(n_train)
 
-	# we want to estimate the function at each of these points 
-	n_test = 100
-	x_test = np.linspace(x[0], x[-1], n_test)
+        # we want to estimate the function at each of these points
+        n_test = 100
+        x_test = np.linspace(x[0], x[-1], n_test)
 
-	gpr(x, y, u, x_test, [1,1])
+        gpr(x, y, u, x_test, [1,1])
 
-	theta = opt_params(logl, x, y, u, x_test, [1,1])
+        theta = opt_params(logl, x, y, u, x_test, [1,1])
 
-	gpr(x, y, u, x_test, theta)
+        gpr(x, y, u, x_test, theta)
 
